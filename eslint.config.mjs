6cf99de6768770bcc0e80763b587391f3e1c1c09
follijulio <follijulio@gmail.com>
2026-03-10 11:05:19 -0300

    chore: update linting configuration and dependencies
    - Changed lint command from "next lint" to "eslint"
    - Removed unused dependencies: "@react-email/components", "@react-email/render", and "resend"
    - Added new dependency: "@radix-ui/react-progress"
    - Added eslint plugin: "eslint-plugin-simple-import-sort"
    - Introduced overrides for "nodemailer"

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,47 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
-import eslintConfigPrettier from "eslint-config-prettier"; // <-- Importamos o Prettier aqui
+import eslintConfigPrettier from "eslint-config-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort"; // <-- O novo plugin
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  eslintConfigPrettier,
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "linebreak-style": ["error", "unix"], // Força o padrão LF (Linux/Mac)
+      "simple-import-sort/exports": "error",
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            // 1. React e pacotes externos (ex: react, next, date-fns)
+            ["^react", "^@?\\w"],
+            // 2. Interfaces do domínio
+            ["^@/domain/interfaces"],
+            // 3. Utilitários
+            ["^@/lib/utils"],
+            // 4. Componentes
+            ["^@/components"],
+            // 5. Mocks
+            ["^(\\.\\./mocks)"],
+            // 6. Imports relativos de pastas pai (../)
+            ["^\\.\\.(?!/?$)", "^\\../?$"],
+            // 7. Imports relativos da mesma pasta (./)
+            ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+            // 8. Ficheiros de estilo (css, scss)
+            ["^.+\\.s?css$"],
+          ],
+        },
+      ],
+    },
+  },
+  eslintConfigPrettier, // Prettier sempre no final para não haver conflitos
 
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
 export default eslintConfig;
